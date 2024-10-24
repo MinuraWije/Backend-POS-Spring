ce461752cdc0d65168e7d88da9ebf8f4f3c84265
MinuraWije <minurawije@gmail.com>
2024-10-24 12:20:26 +0530

    completed order exception class

---
+++ src/main/java/org/example/backendpossystemspring/exception/OrderNotFoundException.java
@@ -0,0 +1,10 @@
+package org.example.backendpossystemspring.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+    }
+    public OrderNotFoundException(String message, Throwable cause) {
+
+    }
+    public OrderNotFoundException() {}
+}
